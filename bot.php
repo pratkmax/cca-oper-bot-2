64ff48d01b9b0f711ed7a13bad6b51731347350a
pratkmax <pratkmax@gmail.com>
2017-02-24 13:44:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'คุณคือใคร' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'สวัสดีครับ CCA BOT ยินดีให้บริการครับ'
+				'text' => 'ดิฉันคือน้อง CCA OPER BOT ค่ะ'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
