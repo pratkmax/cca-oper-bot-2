6fad94ca12d20656b52fab6a8d214f99993a4969
pratkmax <pratkmax@gmail.com>
2018-10-05 11:58:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -37,7 +37,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			$messages = [
 				'type' => 'text',
-				'text' =>  '" . date("Y/m/d") . "สวัสดีครับ ขอให้เป็นวันที่สดใสนะครับ , have a nice day' 
+				'text' =>  '$date("Y/m/d") สวัสดีครับ ขอให้เป็นวันที่สดใสนะครับ , have a nice day' 
 			];
 			  			   
 		   }
